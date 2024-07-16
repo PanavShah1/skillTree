0bcea076a277fce384e75e25f9ec832c4879cc72
Akshatk-me <kushwahakshat32@gmail.com>
2024-07-16 17:41:02 +0530

    added dummy_data

--- dummy_data.py
+++ dummy_data.py
@@ -1,9 +1,18 @@
-# eg. on calling MA614 it should return
-{
-    "MA415": {
-        "MA213": {
-            "MA110" : {}
-        },
-        "MA214": {}
-    }
-}
+class nodule: 
+    def __init__(self, name, member) -> None:
+        #member may also be of type nodule
+        self.name = name
+        self.member = member 
+    def tellme(self):
+        print(self.name, self.member.name)
+
+
+
+C5 = nodule("C5", [[]])
+C6 = nodule("C6", [[]])
+C7 = nodule("C7", [[]])
+C3 = nodule("C3", [[C5]])
+C4 = nodule("C4", [[C6], [C7]])
+C2 = nodule("C2", [[]])
+C1 = nodule("C1", [[C2], [[C3], [C4]]])
+
---
+++ extractor.py
@@ -0,0 +1,8 @@
+import pandas
+df = pandas.read_csv("./pre_req.csv")
+
+
+
+
+
+
