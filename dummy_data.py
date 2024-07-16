9b862ae8f4d3f3de5d6e657d8a625ec8eefbe5fa
PanavShah1 <panav.shah@gmail.com>
2024-07-16 17:06:38 +0530

    added dummy data

---
+++ dummy_data.py
@@ -0,0 +1,9 @@
+# eg. on calling MA614 it should return
+{
+    "MA415": {
+        "MA213": {
+            "MA110" : {}
+        },
+        "MA214": {}
+    }
+}
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./style.css";
+import Tree from "./Tree";
 
 const App = () => {
     return (
         <div>
             <h1>Welcome to SkillTree</h1>
+            <Tree />
         </div>
     )
 }
